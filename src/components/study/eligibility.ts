0deff0a8ea403197d2b14826a2cd16cbc6daa33b
Graeme Zinck <graeme.zinck@gmail.com>
2021-07-28 19:02:44 -0400

    Increase max participants to 12 per group

--- src/components/study/eligibility.ts
+++ src/components/study/eligibility.ts
@@ -1,3 +1,3 @@
 // Increase these numbers to accept more participants to the study
-export const numMusicalParticipants = 11; // One higher because the facilitator did the study once
-export const numNonmusicalParticipants = 10;
+export const numMusicalParticipants = 12; // One higher because the facilitator did the study once
+export const numNonmusicalParticipants = 12; // One higher because supervisor did the study once
