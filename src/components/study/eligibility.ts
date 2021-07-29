20149b9ce5ab990b39f55f6b2b95d985869c858b
Graeme Zinck <graeme.zinck@gmail.com>
2021-07-29 16:07:01 -0400

    Allow double participants to study just in case (funding is OK)

--- src/components/study/eligibility.ts
+++ src/components/study/eligibility.ts
@@ -1,3 +1,3 @@
 // Increase these numbers to accept more participants to the study
-export const numMusicalParticipants = 12; // One higher because the facilitator did the study once
-export const numNonmusicalParticipants = 12; // One higher because supervisor did the study once
+export const numMusicalParticipants = 21; // One higher because the facilitator did the study once
+export const numNonmusicalParticipants = 21; // One higher because supervisor did the study once
